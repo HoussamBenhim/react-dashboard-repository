3a46ab08e3aadd9ffeb59de6c7c5eae826279ef2
Houssam <hbenhim@sequantis.com>
2020-06-29 14:35:26 +0200

    Updates

---
+++ precache-manifest.9c1ca6088dab5f854f3d284894110972.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b07318afe4e0132b8eb24066e3213b4",
+    "url": "/react-dashboard-repository/index.html"
+  },
+  {
+    "revision": "ac0063a2c94ddf2ca107",
+    "url": "/react-dashboard-repository/static/css/main.e42e9bf8.chunk.css"
+  },
+  {
+    "revision": "d7e7652bc706c7b2297e",
+    "url": "/react-dashboard-repository/static/js/2.21dcf826.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-dashboard-repository/static/js/2.21dcf826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac0063a2c94ddf2ca107",
+    "url": "/react-dashboard-repository/static/js/main.91613261.chunk.js"
+  },
+  {
+    "revision": "25fa0bfa9a9cc7b0a7e2",
+    "url": "/react-dashboard-repository/static/js/runtime-main.8a1586de.js"
+  },
+  {
+    "revision": "d19ee3e99a2ed807fb90419aa1b55e06",
+    "url": "/react-dashboard-repository/static/media/logo.d19ee3e9.JPG"
+  }
+]);
